0e16b791829b4568be405be15e61176dc5b91c54
4realjocelyn <simran142004@gmail.com>
2025-06-11 15:27:11 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,25 @@
 
-console.log("Kindblade game logic will go here.");
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
 
-// Placeholder: later we can add character movement, combat, etc.
+let player = {
+  x: 100,
+  y: 100,
+  size: 30,
+  color: "purple",
+  name: "Akaza"
+};
+
+function drawPlayer() {
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.size, player.size);
+  ctx.fillStyle = "black";
+  ctx.fillText(player.name, player.x, player.y - 10);
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawPlayer();
+}
+
+setInterval(draw, 1000 / 60);
