00719995443c22311e6f7f1f2ce16c7888961ac5
4realjocelyn <simran142004@gmail.com>
2025-06-11 19:09:13 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -1,1 +1,25 @@
 
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+let player = {
+  x: 100,
+  y: 100,
+  size: 30,
+  color: "purple",
+  name: "Akaza"
+};
+
+function drawPlayer() {
+  ctx.fillStyle = player.color;
+  ctx.fillRect(player.x, player.y, player.size, player.size);
+  ctx.fillStyle = "black";
+  ctx.fillText(player.name, player.x, player.y - 10);
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawPlayer();
+}
+
+setInterval(draw, 1000 / 60);
